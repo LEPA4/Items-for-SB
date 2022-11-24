ca9ffe58a60a2cfbad67c6e6dacb9e21d539ee3b
Anthony Salvador <antsal07@gmail.com>
2022-11-23 22:08:11 -0500

    Lunch box inventory --- saves and loads on server restart. Next up: Make sure the item is a food item.

--- src/main/java/sbitems/skyblockitems/SkyblockItems.java
+++ src/main/java/sbitems/skyblockitems/SkyblockItems.java
@@ -1,28 +1,63 @@
 package sbitems.skyblockitems;
 
+import org.bukkit.Bukkit;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import sbitems.skyblockitems.events.RegisterEvents;
 import sbitems.skyblockitems.items.ItemManager;
 import sbitems.skyblockitems.items.LunchBox;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
 public final class SkyblockItems extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
+        // Basically reads and adds the items --> calls the class that calls the class that creates items.
+        ItemManager.init();
+
         // Registers the event handlers
         getServer().getPluginManager().registerEvents(new RegisterEvents(), this);
         getServer().getPluginManager().registerEvents(new LunchBox(), this);
 
-        // Basically reads and adds the items --> calls the class that calls the class that creates items.
-        ItemManager.init();
+        // Loads from data file
+        this.saveConfig();
+
+        if(this.getConfig().contains("data"))
+            this.load();
     }
 
     @Override
     public void onDisable() {
+        // Saves to data file
+        if(!LunchBox.PlayerInv.isEmpty())
+            this.save();
         // IDK what this does. I just added it bc it sounded cool.
         HandlerList.unregisterAll((Plugin) this);
+    }
+
+    public void save(){
+        for(UUID user : LunchBox.PlayerInv.keySet()){
+            this.getConfig().set("data." + user, LunchBox.PlayerInv.get(user).getContents());
+        }
+        this.saveConfig();
+    }
+    public void load(){
+        this.getConfig().getConfigurationSection("data").getKeys(false).forEach(key -> {
+            Inventory saveinv = Bukkit.createInventory(null, 36, "Lunch Box");
+            ItemStack[] items = ((List<ItemStack>) this.getConfig().get("data." + key)).toArray(new ItemStack[0]);
+            int index = 0;
+            for(ItemStack i : items){
+                saveinv.setItem(index, i);
+                index++;
+            }
+            LunchBox.PlayerInv.put(UUID.fromString(key), saveinv);
+        });
     }
 }
--- src/main/java/sbitems/skyblockitems/items/LunchBox.java
+++ src/main/java/sbitems/skyblockitems/items/LunchBox.java
@@ -1,12 +1,28 @@
 package sbitems.skyblockitems.items;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.inventory.Inventory;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public class LunchBox implements Listener {
 
+    public static Map<UUID, Inventory> PlayerInv = new HashMap<>();
+
     public static void openLunchBoxGUI(Player p){
-        p.sendMessage("hello");
+        // Checks if player has already used the lunch bock, if so, then create a new one.
+        if(PlayerInv.containsKey(p.getUniqueId()))
+            p.openInventory(PlayerInv.get(p.getUniqueId()));
+        else{
+            Inventory inv = Bukkit.createInventory(p, 36, "Lunch Box");
+            p.openInventory(inv);
+            PlayerInv.put(p.getUniqueId(), inv);
+        }
     }
 
 
